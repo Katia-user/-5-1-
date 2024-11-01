e37988130931828f42360c756ab1785e9388fbcf
Катя <@DESKTOP-8CVJE59>
2024-11-01 09:55:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 8; // Розмірність матриці
+        int[,] matrix = new int[n, n];
+        Random random = new Random();
+
+        // Генерація матриці і виведення її на екран
+        Console.WriteLine("Початкова матриця:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = random.Next(0, 101); // Випадкові числа від 0 до 100
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        // Обробка матриці: заміна елементів головної діагоналі
+        for (int i = 0; i < n; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (i != j && i + j != n - 1) // Перпендикулярні до головної діагоналі
+                {
+                    sum += matrix[j, i];
+                }
+            }
+            matrix[i, i] = sum; // Заміна елементу головної діагоналі
+        }
+
+        // Виведення перетвореної матриці
+        Console.WriteLine("\nПеретворена матриця:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
+
+
